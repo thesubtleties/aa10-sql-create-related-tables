19de9a1ba781b16c2effd0c1a9ad3e41c4ef7baa
Steven <stevenglab@gmail.com>
2024-08-10 18:53:09 -0700

    finished

---
+++ sql-practice/fill.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    bands (name)
+VALUES
+    ("fakeBand1");
+
+INSERT INTO
+    musicians (first_name, last_name, band_id)
+VALUES
+    ("fake", "musician", 1);
+
+INSERT INTO
+    instruments (TYPE)
+VALUES
+    ("harmonica");
+
+INSERT INTO
+    musicians_instruments (musician_id, instrument_id)
+VALUES
+    (1, 1);
